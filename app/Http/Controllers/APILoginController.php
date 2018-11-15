73f5ba892d7291d21e9fcef0cfc7c9a5f22e8a28
septiandrd <septiandrd@gmail.com>
2018-11-15 13:28:53 +0700

    fix login

--- app/Http/Controllers/APILoginController.php
+++ app/Http/Controllers/APILoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Store;
 use Illuminate\Http\Request;
+use Mockery\Exception;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Validator;
 use JWTFactory;
@@ -14,32 +15,47 @@ class APILoginController extends Controller
 {
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255',
-            'password'=> 'required'
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors());
-        }
-        $credentials = $request->only('email', 'password');
         try {
-            if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 200);
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|string|email|max:255',
+                'password'=> 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors());
             }
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'could_not_create_token'], 500);
+
+            $code = "FAILED";
+            $description = "OK";
+            return response()->json(compact('code','description'));
+
+        } catch (Exception $exception) {
+            $code = "FAILED";
+            $description = $exception;
+            return response()->json(compact('code','description'));
         }
-
-        try {
-            $user = User::where('email',$request->email)->first();
-            $user->remember_token = $token;
-            $user->save();
-            $store = Store::where('user_id',$user->id)->get();
-
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'invalid_credentials'], 200);
-        }
-
-        return response()->json(compact('token','user','store'),200);
+//
+//        $credentials = $request->only('email', 'password');
+//
+//
+//        try {
+//            if (! $token = JWTAuth::attempt($credentials)) {
+//                return response()->json(['error' => 'invalid_credentials'], 200);
+//            }
+//        } catch (JWTException $e) {
+//            return response()->json(['error' => 'could_not_create_token'], 500);
+//        }
+//
+//        try {
+//            $user = User::where('email',$request->email)->first();
+//            $user->remember_token = $token;
+//            $user->save();
+//            $store = Store::where('user_id',$user->id)->get();
+//
+//        } catch (JWTException $e) {
+//            return response()->json(['error' => 'invalid_credentials'], 200);
+//        }
+//
+//        return response()->json(compact('token','user','store'),200);
     }
 }
