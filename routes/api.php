6473212e130187a1757c2ed873ef30932d3d77bc
septiandrd <septiandrd@gmail.com>
2018-11-15 12:47:57 +0700

    fix import login

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::middleware('jwt.auth')->get('/users', function(Request $request) {
     return auth()->user();
 });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('api.auth')->get('/user', function (Request $request) {
     return $request->user();
 });
 
