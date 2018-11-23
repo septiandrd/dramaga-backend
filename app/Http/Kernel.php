c0a29f2e33cd21dacb7bfbae6daee90f01c882e6
septiandrd <septiandrd@gmail.com>
2018-11-23 15:30:44 +0700

    delete middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,6 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
             \Barryvdh\Cors\HandleCors::class,
-            'checkHeader'
         ],
     ];
 
