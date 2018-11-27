923efb49a5e39675399a8b5587188905e6c70fac
septiandrd <septiandrd@gmail.com>
2018-11-27 14:26:22 +0700

    change this

--- routes/api.php
+++ routes/api.php
@@ -57,5 +57,7 @@ Route::group(['middleware' => ['checkHeader']], function () {
 
 Route::get('product/img', 'APIProductController@getImage');
 
+
+# change this
 Route::get('transaction/confirmPayment','APITransactionController@confirmPayment');
 Route::get('transaction/confirmPaymentGet','APITransactionController@confirmGet');
